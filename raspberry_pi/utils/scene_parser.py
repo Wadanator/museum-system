ce0f342b2b81f74805f3a66dd91f241b0e67c1bc
Andrej Žabka <257720@vutbr.cz>
2025-12-19 11:07:10 +0100

    Update scene_parser.py

--- raspberry_pi/utils/scene_parser.py
+++ raspberry_pi/utils/scene_parser.py
@@ -1,7 +1,4 @@
 #!/usr/bin/env python3
-"""
-Scene Parser - Riadi vykonávanie scény
-"""
 from utils.logging_setup import get_logger
 from utils.state_machine import StateMachine
 from utils.transition_manager import TransitionManager
@@ -23,14 +20,12 @@ class SceneParser:
         self.scene_data = None
 
     def load_scene(self, scene_file):
-        """Načíta scénu cez State Machine"""
         if self.state_machine.load_scene(scene_file):
             self.scene_data = True
             return True
         return False
 
     def start_scene(self):
-        """Spustí načítanú scénu"""
         if not self.scene_data:
             self.logger.error("No scene data loaded")
             return False
@@ -39,7 +34,6 @@ class SceneParser:
         self.state_executor.reset_timeline_tracking()
         
         if self.state_machine.start():
-            # Okamžite vykonaj onEnter pre prvý stav
             current_data = self.state_machine.get_current_state_data()
             if current_data:
                 self.state_executor.execute_onEnter(current_data)
@@ -47,7 +41,6 @@ class SceneParser:
         return False
 
     def process_scene(self):
-        """Hlavná slučka vykonávania scény"""
         if self.state_machine.is_finished():
             return False
 
@@ -55,11 +48,9 @@ class SceneParser:
         if not current_state_data:
             return False
 
-        # 1. KONTROLA GLOBÁLNYCH EVENTOV (Priorita)
         global_events = self.state_machine.get_global_events()
         if global_events:
             global_context = {"transitions": global_events}
-            # Pre globálne eventy používame čas od začiatku scény
             scene_elapsed = self.state_machine.get_scene_elapsed_time()
             
             next_state_global = self.transition_manager.check_transitions(
@@ -72,30 +63,27 @@ class SceneParser:
                 self._change_state(next_state_global, current_state_data)
                 return True
 
-        # 2. Bežná kontrola Timeline
         state_elapsed = self.state_machine.get_state_elapsed_time()
         self.state_executor.check_and_execute_timeline(current_state_data, state_elapsed)
 
-        # 3. Bežná kontrola Prechodov
         next_state = self.transition_manager.check_transitions(current_state_data, state_elapsed)
         if next_state:
             self._change_state(next_state, current_state_data)
             
         return True
 
+    def register_mqtt_event(self, topic, payload):
+        """Zaregistruje prichádzajúci MQTT event pre TransitionManager (Oprava chyby)"""
+        if self.transition_manager:
+            self.transition_manager.register_event(topic, payload)
+            self.logger.debug(f"MQTT event registered: {topic} = {payload}")
+
     def _change_state(self, next_state, current_state_data):
-        """Vykoná bezpečný prechod medzi stavmi"""
-        # 1. onExit starého stavu
         self.state_executor.execute_onExit(current_state_data)
-        
-        # 2. Zmena stavu
         self.state_machine.goto_state(next_state)
-        
-        # 3. Vyčistenie eventov a timeline pre nový stav
         self.transition_manager.clear_events()
         self.state_executor.reset_timeline_tracking()
         
-        # 4. onEnter nového stavu
         new_state_data = self.state_machine.get_current_state_data()
         if new_state_data:
             self.state_executor.execute_onEnter(new_state_data)
